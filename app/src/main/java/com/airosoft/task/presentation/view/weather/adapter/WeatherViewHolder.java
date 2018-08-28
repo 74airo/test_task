1d77391d91bc221645d1943cdfdad9d3c0d79f77
Igor Pirozhok <74tairo@gmail.com>
2018-08-28 03:45:59 +0300

    improved view of the weather list item

--- app/src/main/java/com/airosoft/task/presentation/view/weather/adapter/WeatherViewHolder.java
+++ app/src/main/java/com/airosoft/task/presentation/view/weather/adapter/WeatherViewHolder.java
@@ -10,7 +10,12 @@ import com.airosoft.task.R;
 import com.airosoft.task.model.local.WeatherModel;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import butterknife.BindDimen;
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -29,6 +34,18 @@ public class WeatherViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.condition_icon_image_view) ImageView conditionIconImageView;
 
     @BindDimen(R.dimen.icon_size_large) int iconSize;
+    @BindString(R.string.pattern_date_time) String patternDateTime;
+    @BindString(R.string.pattern_temp) String patternTemp;
+    @BindString(R.string.pattern_wind) String patternWind;
+    @BindString(R.string.pattern_pressure) String patternPressure;
+    @BindString(R.string.pattern_humidity) String patternHumidity;
+    @BindString(R.string.pattern_cloudiness) String patternCloudiness;
+
+    @BindString(R.string.weather_unit_temp_celsius) String unitTempCelsius;
+    @BindString(R.string.weather_unit_pressure) String unitPressure;
+    @BindString(R.string.weather_unit_humidity) String unitHumidity;
+    @BindString(R.string.weather_unit_cloudiness) String unitCloudiness;
+    @BindString(R.string.weather_unit_wind) String unitWind;
 
     private WeatherModel weather;
 
@@ -40,23 +57,43 @@ public class WeatherViewHolder extends RecyclerView.ViewHolder {
     public void onBind(WeatherModel weather) {
         this.weather = weather;
 
-        dateRequestTextView.setText(String.format("%d", weather.getDateRequest()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat(patternDateTime, Locale.US);
+
+        dateRequestTextView.setText(dateFormat.format(new Date(weather.getDateRequest())));
         setConditionData();
         setMainData();
         setWindData();
     }
 
     private void setWindData() {
-        windTextView.setText(String.format("%.1f, %.1f", weather.getWind().getSpeed(), weather.getWind().getDegree()));
+        windTextView.setText(String.format(
+                patternWind,
+                Math.round(weather.getWind().getSpeed()),
+                unitWind,
+                Math.round(weather.getWind().getDegree()) + "\u00B0" //unicode of degree symbol
+        ));
     }
 
     private void setMainData() {
-        tempTextView.setText(String.valueOf(weather.getMainData().getTemp()));
-        tempMinTextView.setText(String.valueOf(weather.getMainData().getTempMin()));
-        tempMaxTextView.setText(String.valueOf(weather.getMainData().getTempMax()));
-        humidityTextView.setText(String.valueOf(weather.getMainData().getHumidity()));
-        pressureTextView.setText(String.valueOf(weather.getMainData().getPressure()));
-        cloudinessTextView.setText(String.valueOf(weather.getMainData().getCloudiness()));
+        cloudinessTextView.setText(String.format(
+                patternCloudiness, weather.getMainData().getCloudiness(), unitCloudiness));
+        humidityTextView.setText(String.format(
+                patternHumidity, weather.getMainData().getHumidity(), unitHumidity));
+        pressureTextView.setText(String.format(
+                patternPressure, weather.getMainData().getPressure(), unitPressure));
+        setTemp(tempTextView, (int)Math.round(weather.getMainData().getTemp()));
+        setTemp(tempMinTextView, (int)Math.round(weather.getMainData().getTempMin()));
+        setTemp(tempMaxTextView, (int)Math.round(weather.getMainData().getTempMax()));
+    }
+
+    private void setTemp(TextView textView, int temp) {
+        if (temp > 0) {
+            textView.setText(String.format(patternTemp, "+", temp, unitTempCelsius));
+        } else if (temp < 0) {
+            textView.setText(String.format(patternTemp, "-", temp, unitTempCelsius));
+        } else {
+            textView.setText(String.format(patternTemp, "", temp, unitTempCelsius));
+        }
     }
 
     private void setConditionData() {
